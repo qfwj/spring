8e950c7911d140a880d1cded3587d3a093bb1802
qf <13554254100@163.com>
2017-09-25 16:27:17 +0800

    Http proxy

---
+++ SpringBOOT/HttpProxy/src/main/java/com/qf/httpproxy/HttpproxyApplication.java
@@ -0,0 +1,12 @@
+package com.qf.httpproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpproxyApplication.class, args);
+	}
+}
