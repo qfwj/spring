4fec52c39006ed70fa05d54f39797df9bd444e34
qf <13554254100@163.com>
2017-12-12 19:23:27 +0800

    kafkastreams  test

---
+++ SpringBOOT/TestKafkaStreams/src/main/java/com/qf/kafkastreams/testkafkastreams/TestkafkastreamsApplication.java
@@ -0,0 +1,12 @@
+package com.qf.kafkastreams.testkafkastreams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestkafkastreamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestkafkastreamsApplication.class, args);
+	}
+}
---
+++ SpringBOOT/TestKafkaStreams/src/main/java/com/qf/kafkastreams/testkafkastreams/service/WordsCountService.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright(c) 2013-2017 by Puhuifinance Inc.
+ * All Rights Reserved
+ */
+package com.qf.kafkastreams.testkafkastreams.service;
+
+import kafka.consumer.KafkaStream;
+
+/**
+ * @ClassName: WordsCountService
+ * @Description:
+ * @author: zhbo
+ * @date: 2017/12/12 19:11
+ * @Copyright: 2017 . All rights reserved.
+ */
+public class WordsCountService {
+    KafkaStream
+}
